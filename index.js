9468e4d8ee1881916e36206a825561c38c9d1531
Ramiz Ali <ramiz.apk7@gmail.com>
2022-12-30 04:29:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1=31;
+let num2=2;
+let num3=49;
+let num4=5;
 
-// Write your code here
+let multiply = num1*num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max=Math.max(1, 2, 3, 4, 5,10,20);
